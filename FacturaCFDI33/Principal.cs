131f176e04971d7370ca8e09034b26b431f34c35
Hugo Contreras <h_castillo13@live.com.mx>
2018-06-03 22:39:07 -0500

    ok

---
+++ FacturaCFDI33/Principal.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace FacturaCFDI33
+{
+    class Principal
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
